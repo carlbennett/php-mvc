5cf51c06f8f2a0a9aaf0d5ab047111036d9a5dd2
Carl Bennett <carlbennett@users.noreply.github.com>
2015-12-11 00:24:12 +0000

    Add Pair library unit test

---
+++ tests/Pair.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?php
+
+namespace CarlBennett\MVC\Tests\HTTPHeader {
+
+  use \CarlBennett\MVC\Libraries\Pair;
+
+  function main($argc, $argv) {
+    require_once("./_loader.php");
+    assert_options(ASSERT_CALLBACK, function(){
+      echo "Unit test failed: " . __FILE__ . "\n";
+      exit(1);
+    });
+
+    $obj = new Pair("foo", "bar");
+    assert($obj->getKey()   === "foo");
+    assert($obj->getValue() === "bar");
+    unset($obj);
+
+    return 0;
+  }
+
+  exit(main($argc, $argv));
+}
