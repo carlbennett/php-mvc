3216d5f2ee681b4712ff828070d36042dae4e616
Carl Bennett <carl@carlbennett.me>
2016-08-24 07:13:41 +0000

    Software License Changes
    The Creative Commons Attribution-ShareAlike 4.0 International
    ("CC BY-SA 4.0") license originally used with the php-mvc software and
    source code is not valid for software licensing. Since the original
    license is not valid for software licensing, the copyright holder is now
    upgrading the license from its original version.
    
    As of August 24, 2016, the php-mvc software will be licensed under the
    GNU Affero General Public License v3 ("AGPLv3").
    
    All previously distributed copies and modifications of this software are
    hereby licensed under the AGPLv3 license retroactively.

--- src/libraries/Exceptions/ViewNotFoundException.php
+++ src/libraries/Exceptions/ViewNotFoundException.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ *  php-mvc, a PHP micro-framework for use as a frontend and/or backend
+ *  Copyright (C) 2015-2016  Carl Bennett
+ *  This file is part of php-mvc.
+ *
+ *  php-mvc is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  php-mvc is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with php-mvc.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace CarlBennett\MVC\Libraries\Exceptions;
 
