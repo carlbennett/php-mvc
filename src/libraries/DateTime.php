337fb5987fc6a97d8e971b0a4ca229962ede7eb4
Carl Bennett <carl@carlbennett.me>
2016-12-29 01:56:45 +0000

    Initial commit for DateTime changes

--- src/libraries/DateTime.php
+++ src/libraries/DateTime.php
@@ -2,7 +2,10 @@
 
 namespace CarlBennett\MVC\Libraries;
 
-class DateTime extends \DateTime implements \JsonSerializable {
+use \LogicException;
+use \OutOfBoundsException;
+
+class DateTime extends \DateTime implements \JsonSerializable, \ArrayAccess {
 
   public function jsonSerialize() {
     return [
@@ -11,6 +14,29 @@ class DateTime extends \DateTime implements \JsonSerializable {
       'sql'  =>       $this->format('Y-m-d H:i:s.u'),
       'unix' => (int) $this->format('U'),
     ];
+  }
+
+  public function offsetExists($offset) {
+    return array_key_exists($offset, [
+      'iso'  => null,
+      'tz'   => null,
+      'sql'  => null,
+      'unix' => null,
+    ]);
+  }
+
+  public function offsetGet($offset) {
+    switch ($offset) {
+      case 'iso':  return       $this->format(DATE_RFC2822);
+      case 'tz':   return       $this->format('T');
+      case 'sql':  return       $this->format('Y-m-d H:i:s.u');
+      case 'unix': return (int) $this->format('U');
+    }
+    throw new OutOfBoundsException();
+  }
+
+  public function offsetSet($offset, $value) {
+    throw new LogicException(
   }
 
 }
