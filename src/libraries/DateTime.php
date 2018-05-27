4e0f91fa86d508815182478abb1e1965bf1bc934
Carl Bennett <carl@carlbennett.me>
2018-05-27 17:36:21 -0500

    Update DateTime object with latest from bnetdocs

--- src/libraries/DateTime.php
+++ src/libraries/DateTime.php
@@ -2,15 +2,31 @@
 
 namespace CarlBennett\MVC\Libraries;
 
-class DateTime extends \DateTime implements \JsonSerializable {
+use \DateTime as PHPDateTime;
+use \JsonSerializable;
 
+class DateTime extends PHPDateTime implements JsonSerializable {
+
+  const STRING_FORMAT = 'r';
+
+  /**
+   * jsonSerialize()
+   * Part of the JsonSerializable interface.
+   * Changes the representation of this object when using json.
+   *
+   * @return string The data to be returned to json_encode().
+   */
   public function jsonSerialize() {
-    return [
+    return array(
       'iso'  =>       $this->format(DATE_RFC2822),
-      'tz'   =>       $this->format('T'),
       'sql'  =>       $this->format('Y-m-d H:i:s.u'),
+      'tz'   =>       $this->format('T'),
       'unix' => (int) $this->format('U'),
-    ];
+    );
+  }
+
+  public function __toString() {
+    return $this->format( self::STRING_FORMAT );
   }
 
 }
