86918e42b7ed7f8dafec9a49aa431f2a005ebee3
Carl Bennett <carl@carlbennett.me>
2020-02-07 03:23:29 -0600

    Extend PHP's DateTime.format() with timezone param

--- src/libraries/DateTime.php
+++ src/libraries/DateTime.php
@@ -10,6 +10,26 @@ class DateTime extends PHPDateTime implements JsonSerializable {
   const STRING_FORMAT = 'r';
 
   /**
+   * format()
+   * Extends the native format() function of PHP's DateTime class.
+   * The second parameter allows converting to another timezone on the fly.
+   *
+   * @param $format The date format, passed into PHP's DateTime.format()
+   * @param DateTimeZone $tz Optional timezone to convert to before formatting.
+   *
+   * @return string The formatted string optionally converted to a timezone.
+   */
+  public function format($format, DateTimeZone $tz = null) {
+    if (!$tz) {
+      return parent::format($format);
+    } else {
+      $dt = clone $this;
+      $dt->setTimezone($tz);
+      return $dt->format($format);
+    }
+  }
+
+  /**
    * jsonSerialize()
    * Part of the JsonSerializable interface.
    * Changes the representation of this object when using json.
