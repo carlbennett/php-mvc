dc09d2abf903ced68cc227ca5cbd0fd2284a5b71
Carl Bennett <carl@carlbennett.me>
2022-10-01 05:57:49 -0500

    Fix test autoloader

--- tests/_loader.php
+++ tests/_loader.php
@@ -1,21 +1,17 @@
 <?php
 
-namespace CarlBennett\MVC {
+namespace CarlBennett\MVC;
 
-  spl_autoload_register(function($className){
+\spl_autoload_register(function($className)
+{
     $path = $className;
-    if (substr($path, 0, 15) == "CarlBennett\\MVC") $path = substr($path, 16);
-    $cursor = strpos($path, "\\");
-    if ($cursor !== false) {
-      $path = strtolower(substr($path, 0, $cursor)) . substr($path, $cursor);
-    }
-    $path = str_replace("\\", "/", $path);
+    if (\substr($path, 0, 15) == "CarlBennett\\MVC") $path = \substr($path, 16);
+    $path = \str_replace("\\", \DIRECTORY_SEPARATOR, $path);
     $classShortName = $path;
-    $path = "../src/" . $path . ".php";
-    if (!file_exists($path)) {
-      trigger_error("Class not found: " .$classShortName, E_USER_ERROR);
+    $path = '../src/' . $path . '.php';
+    if (!\file_exists($path))
+    {
+        \trigger_error('Class not found: ' . $classShortName, \E_USER_ERROR);
     }
     require_once($path);
-  });
-
-}
+});
